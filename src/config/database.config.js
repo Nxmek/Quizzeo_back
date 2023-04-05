079d342964ab7164a47fdb13f19c0b6461bb58b8
Nxmek <valentin.laveau@gmail.com>
2023-04-05 10:22:12 +0200

    Bdd en ligne

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-// const uri = "mongodb://ockode:ockodepassmdp@mongodb:27017";
-const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
-
+const uri = process.env.URI || "mongodb://localhost:27017";
+console.log(uri);
 const initDb = async () => {
   try {
     mongoose.set("strictQuery", false);
-    await new mongoose.connect(uri, { dbName: "Quizzeo" });
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected");
   } catch (e) {
     console.log("oh oh problem : ", e.message);
