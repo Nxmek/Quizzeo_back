3237da2f808fd0401f46a796fad9c36b529ccaef
Nxmek <valentin.laveau@gmail.com>
2023-04-05 19:43:55 +0200

    test routes vercel

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,12 +4,31 @@ import { jwtMiddleware } from "../middlewares/jwt.middleware.js";
 
 const initUsersRoutes = (app, sm) => {
   const router = Router();
-  router.get("/read", sm, UserController.read);
-  // router.get("/read/:id", sm, UserController.readUserById);
-  router.get("/readById", sm, jwtMiddleware, UserController.readUserById);
+  // router.get("/read", sm, UserController.read);
+  // // router.get("/read/:id", sm, UserController.readUserById);
+  // router.get("/readById", sm, jwtMiddleware, UserController.readUserById);
 
-  router.post("/sign-up", sm, UserController.signUp);
-  router.post("/sign-in", sm, UserController.signIn);
+  // router.post("/sign-up", sm, UserController.signUp);
+  // router.post("/sign-in", sm, UserController.signIn);
+  router.get("https://quizzeo-back.vercel.app/read", sm, UserController.read);
+  // router.get("/read/:id", sm, UserController.readUserById);
+  router.get(
+    "https://quizzeo-back.vercel.app/readById",
+    sm,
+    jwtMiddleware,
+    UserController.readUserById
+  );
+
+  router.post(
+    "https://quizzeo-back.vercel.app/sign-up",
+    sm,
+    UserController.signUp
+  );
+  router.post(
+    "https://quizzeo-back.vercel.app/sign-in",
+    sm,
+    UserController.signIn
+  );
 
   app.use("/users", router);
 };
