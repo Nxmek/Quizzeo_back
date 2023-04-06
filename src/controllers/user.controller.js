c2cac8edbdcf34eacae739817e6ecf7280917504
Nxmek <valentin.laveau@gmail.com>
2023-04-06 11:41:08 +0200

    testt

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,6 +8,9 @@ import { stringIsFilled } from "../utils/string.utils.js";
 const signUp = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  app.get("/sign-up", (req, res) => {
+    res.send("ca marche ! ");
+  });
 
   if (!emailIsValid(email.toLowerCase())) {
     return res.status(400).json({ message: "invalid_email" });
